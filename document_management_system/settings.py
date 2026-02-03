d19a32644f399b3a65366134e2e1fabe49fd8ba0
Dattatraya Walunj <drwalunj.2010@gmail.com>
2026-02-03 19:17:22 +0530

    Fix STATICFILES_DIRS path

--- document_management_system/settings.py
+++ document_management_system/settings.py
@@ -150,9 +150,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = (
-    os.path.join(STATIC_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(STATIC_ROOT, 'static'),
+# )
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',   # ✅ must exist
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
